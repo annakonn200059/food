157147a14401f18b77ef48def6d801c85094fb0d
Ann <konnova.anya2016@yandex.ru>
2020-08-17 21:49:53 +0400

    rest оператор

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -148,35 +148,42 @@ function showModalByScroll() {
 
     //используем классы для карточек
 
-    class MenuCard{
-        constructor(src, alt, title, descr, price, parentSelector) {
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
-            this.changeToUAH();
+            this.changeToUAH(); 
         }
 
         changeToUAH() {
-            this.price = this.price * this.transfer;
+            this.price = this.price * this.transfer; 
         }
 
         render() {
             const element = document.createElement('div');
+
+            if (this.classes.length === 0) {
+                this.classes = "menu__item";
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `
-                    <div class="menu__item">
-                            <img src=${this.src} alt=${this.alt}>
-                            <h3 class="menu__item-subtitle">${this.title}</h3>
-                            <div class="menu__item-descr">${this.descr}</div>
-                            <div class="menu__item-divider"></div>
-                            <div class="menu__item-price">
-                                <div class="menu__item-cost">Цена:</div>
-                                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                            </div>
-                        </div>
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
             `;
             this.parent.append(element);
         }
@@ -189,7 +196,7 @@ function showModalByScroll() {
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
         ".menu .container"
-    ).render();//если используем один раз
+    ).render();
 
     new MenuCard(
         "img/tabs/post.jpg",
@@ -208,7 +215,6 @@ function showModalByScroll() {
         21,
         ".menu .container"
     ).render();
-    
 
 });
 
